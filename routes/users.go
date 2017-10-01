a064189017ad412f75bafefdd3e7061188bb40d1
DArcops <diego.lopez.arcos@gmail.com>
2017-09-30 21:02:23 -0500

    endpoints to register and login x 2

--- routes/users.go
+++ routes/users.go
@@ -10,6 +10,6 @@ var users *gin.RouterGroup
 func userRoutes() {
 	users = v1.Group("users")
 	users.GET("", V1.GetUsers)
-	users.POST("/register", V1.RegisterUser)
-
+	users.POST("/register", V1.Register)
+	users.POST("/login", V1.Login)
 }
