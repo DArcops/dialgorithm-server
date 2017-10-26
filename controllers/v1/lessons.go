e44a26bc3a5420a9550ae29c7b997ddf5acec351
darcops <diego.lopez.arcos@gmail.com>
2017-10-25 21:10:21 -0500

    related lessons

--- controllers/v1/lessons.go
+++ controllers/v1/lessons.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/darcops/dialgorithm-server/models"
 	"github.com/gin-gonic/gin"
@@ -37,6 +38,33 @@ func GetLessons(c *gin.Context) {
 	levelID := c.Query("level_id")
 
 	lessons, err := models.GetLessons(levelID)
+	if err != nil {
+		Respond(http.StatusInternalServerError, gin.H{}, c)
+		return
+	}
+	Respond(http.StatusOK, lessons, c)
+	return
+}
+
+func GetRelatedLessons(c *gin.Context) {
+
+	user := c.MustGet("user").(models.User)
+
+	course_id := c.Query("course_id")
+	level_number := c.Query("level_number")
+
+	if models.First(&models.Subscription{}, "course_id = ? and user_id = ?", course_id, user.ID).RecordNotFound() {
+		Respond(http.StatusForbidden, gin.H{"error": "You are not in this course"}, c)
+		return
+	}
+
+	level := models.Level{}
+	if models.First(&level, "course_id = ? and number = ?", course_id, level_number).RecordNotFound() {
+		Respond(http.StatusNotFound, gin.H{}, c)
+		return
+	}
+
+	lessons, err := models.GetLessons(strconv.FormatUint(uint64(level.ID), 10))
 	if err != nil {
 		Respond(http.StatusInternalServerError, gin.H{}, c)
 		return
